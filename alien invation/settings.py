facf5e7c4c71149253ebcba3e45dffc0a9572e52
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-02-22 20:43:57 +0200

    2012/02/22

--- alien invation/settings.py
+++ alien invation/settings.py
@@ -7,10 +7,17 @@ class Settings:
 		self.screen_height = 300
 		self.bg_color = (255, 255, 255)
 		# Ship settings
+		self.ship_limit = 3
 		self.ship_speed = 3
 		# Bullet settings
-		self.bullet_speed = 1.0
+		self.bullet_speed = 1.5
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
-		self.bullets_allowed = 10
+		self.bullets_allowed = 3
+		# Alien settings
+		self.alien_speed = 1.0
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_drop_speed = 10
+		self.fleet_direction = 1
+
---
+++ chapter7/try_it_yourself_chap7.py
@@ -0,0 +1,6 @@
+print("hello")
+
+for x in xrange(1,10):
+	print("Hello")
+
+	
